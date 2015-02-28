145d09f3ef994fe7eac95f0af2a73467e7189283
jittagorn pitakmetagoon <jittagornp@gmail.com>
2015-02-28 09:49:30 +0700

    add ORM

---
+++ mvc/orm/resource-local/src/main/java/com/blogspot/na5cent/resourcelocal/model/Employee.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.blogspot.na5cent.resourcelocal.model;
+
+/**
+ *
+ * @author anonymous
+ */
+public class Employee {
+    
+}
