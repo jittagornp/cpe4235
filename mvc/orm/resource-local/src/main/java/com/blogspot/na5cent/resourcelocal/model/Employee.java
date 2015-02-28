92bd55894ec7f4cd790c0820c91d3faeab6bdc4c
jittagorn pitakmetagoon <jittagornp@gmail.com>
2015-02-28 10:28:39 +0700

    remove unnecessary attribute

--- mvc/orm/resource-local/src/main/java/com/blogspot/na5cent/resourcelocal/model/Employee.java
+++ mvc/orm/resource-local/src/main/java/com/blogspot/na5cent/resourcelocal/model/Employee.java
@@ -27,9 +27,6 @@ public class Employee implements Serializable {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    private String email;
-    @Column(name = "phone_number")
-    private String phoneNumber;
 
     public Integer getId() {
         return id;
@@ -53,22 +50,6 @@ public class Employee implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     @Override
